56fced9d2f1b1cdc27dfe67fbc4961bbb87a3116
Карен <bezim@DESKTOP-IM0G7Q6>
2018-01-31 17:48:01 +0200

    Подсчет количества ссылок на СИИ реализован Отладка дизайна

--- AddingLinks/MainWindow.xaml.cs
+++ AddingLinks/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
+using System.Text.RegularExpressions;
 
 namespace AddingLinks
 {
@@ -23,6 +24,7 @@ namespace AddingLinks
     /// </summary>
     public partial class MainWindow : Window
     {
+        static string text;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,9 +32,41 @@ namespace AddingLinks
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            CreateWordprocessingDocument(@"C:\Users\bezim\Desktop\Invoice.docx");
+            // CreateWordprocessingDocument(@"C:\Users\bezim\Desktop\Invoice.docx"); создание файла
+        
+            string strDoc = @"C:\Users\bezim\Desktop\dsa.docx"; 
+            string strTxt = "Append text in body - OpenAndAddTextToWordDocument";
+            OpenAndAddTextToWordDocument(strDoc, strTxt);
+            string pattern = @"\[(\d|\d\d|\d\d\d|\d\d\d\d)\]";
+            Regex regex = new Regex(pattern);
+            int counter = 0;
+            foreach (Match match in regex.Matches(text))
+            {
+                counter++;
+            }
+            this.label1.Text = "В тексте найдено "+counter.ToString()+" ссылок на список использованных источников";
+            this.label1.Visibility = Visibility.Visible;
         }
-        public static void CreateWordprocessingDocument(string filepath)
+        public static void OpenAndAddTextToWordDocument(string filepath, string txt) // добавление текста в существующий файл
+        {
+            // Open a WordprocessingDocument for editing using the filepath.
+            WordprocessingDocument wordprocessingDocument =
+                WordprocessingDocument.Open(filepath, true);
+
+            // Assign a reference to the existing document body.
+            Body body = wordprocessingDocument.MainDocumentPart.Document.Body;
+
+            // Add new text.
+            //text = body.InnerXml;
+            text = body.InnerText;
+            DocumentFormat.OpenXml.Wordprocessing.Paragraph para = body.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Paragraph());
+            DocumentFormat.OpenXml.Wordprocessing.Run run = para.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Run());
+            run.AppendChild(new Text(txt));
+            
+            // Close the handle explicitly.
+            wordprocessingDocument.Close();
+        }
+        public static void CreateWordprocessingDocument(string filepath) // создание файла
         {
             // Create a document by supplying the filepath. 
             using (WordprocessingDocument wordDocument =
