303440f53ac418f3e4ceb95a9b9bd1a7cd9eab09
Карен <bezim@DESKTOP-IM0G7Q6>
2018-02-01 18:48:00 +0200

    Release

--- AddingLinks/MainWindow.xaml.cs
+++ AddingLinks/MainWindow.xaml.cs
@@ -1,23 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using DocumentFormat.OpenXml;
+﻿using System.Windows;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using System.Text.RegularExpressions;
 using System.Collections;
 using System.IO;
+using Microsoft.Win32;
 
 namespace AddingLinks
 {
@@ -27,7 +14,6 @@ namespace AddingLinks
     public partial class MainWindow : Window
     {
         static string text;
-        static byte[] XML;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,14 +21,19 @@ namespace AddingLinks
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string strDoc = @"C:\Users\bezim\Desktop\aaa.docx"; 
-            FileStream file = new FileStream(@"C:\Users\bezim\Desktop\e.txt", FileMode.Open);
-            XML = new byte[file.Length];
-            file.Read(XML, 0, XML.Length);
-            AddTheListOfSourcesUsed(strDoc);
+            if (!File.Exists(filePath1.Text))
+            {
+                MessageBox.Show("Указан неверный путь к файлу", "Неверный путь", MessageBoxButton.OK, MessageBoxImage.Error);
+                if (!IsOpenFileDialog())
+                {
+                    this.Close();
+                }
+            }
+
+            AddTheListOfSourcesUsed(this.filePath1.Text);
             string pattern = @"\[(\d|\d\d|\d\d\d|\d\d\d\d)\]";
             Regex regex = new Regex(pattern);
-            int differentMatches = 0;          
+            int differentMatches = 0;
             ArrayList array = new ArrayList();
             foreach (Match match in regex.Matches(text))
             {
@@ -50,30 +41,44 @@ namespace AddingLinks
                 {
                     differentMatches++;
                     array.Add(match.ToString());
-                    Console.WriteLine(match);
                 }
             }
-            this.label1.Text = "В тексте найдено " + differentMatches.ToString() + 
-                " различных ссылок на список использованных источников";
-            this.label1.Visibility = Visibility.Visible;
+            Window1 window = new Window1(differentMatches, filePath1.Text);
+            window.Show();
+            this.Close();
         }
 
-        public static void AddTheListOfSourcesUsed(string filepath)
+        public void AddTheListOfSourcesUsed(string filepath)
         {
+            if (!File.Exists(filePath1.Text)) Application.Current.Shutdown();
             WordprocessingDocument wordprocessingDocument =
-                WordprocessingDocument.Open(filepath, true);
-
+                    WordprocessingDocument.Open(filepath, true);
             Body body = wordprocessingDocument.MainDocumentPart.Document.Body;
 
             DocumentFormat.OpenXml.Wordprocessing.Paragraph para = body.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Paragraph());
-            DocumentFormat.OpenXml.Wordprocessing.Run run = para.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Run());
-
+                DocumentFormat.OpenXml.Wordprocessing.Run run = para.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Run());
             text = body.InnerText;
 
-            string result = System.Text.Encoding.UTF8.GetString(XML);
-            body.InnerXml += result;
-
             wordprocessingDocument.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            IsOpenFileDialog();
+        }
+        private bool IsOpenFileDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "Документы Word (*.docx)|*.docx|(*.doc)|*.doc";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                this.filePath1.Text = openFileDialog.FileName;
+                return true;
+            }
+            return false;
         }
     }
 }
---
+++ AddingLinks/Window1.xaml.cs
@@ -0,0 +1,130 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AddingLinks
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        static byte[] XML;
+        int currentIndex = 1;
+        int totalIndexes;
+        string filepath;
+        string res;
+
+        public Window1(int indexes, string fp)
+        {
+            InitializeComponent();
+            totalIndexes = indexes;
+            filepath = fp;
+            Title = indexes + " ссылок";
+            sourceXFromX.Content = "Формарование ссылки 1 из " + indexes;
+
+            FileStream file = new FileStream(@"XML\first.xml", FileMode.Open);
+            XML = new byte[file.Length];
+            file.Read(XML, 0, XML.Length);
+            file.Close();
+            res = System.Text.Encoding.UTF8.GetString(XML);
+
+        }
+        void DataWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if(totalIndexes != currentIndex)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                thirdLabel.Content = "Гиперссылка";
+                fourthBox.Visibility = Visibility.Hidden;
+                fifthBox.Visibility = Visibility.Hidden;
+                fourthLabel.Visibility = Visibility.Hidden;
+                fifthLabel.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                thirdLabel.Content = "Автор(-ы)";
+                fourthBox.Visibility = Visibility.Visible;
+                fifthBox.Visibility = Visibility.Visible;
+                fourthLabel.Visibility = Visibility.Visible;
+                fifthLabel.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sourceXFromX.Content = "Формарование ссылки " + ++currentIndex + " из " + totalIndexes;
+
+            FileStream file = new FileStream(@"XML\item.xml", FileMode.Open);
+            XML = new byte[file.Length];
+            file.Read(XML, 0, XML.Length);
+            string tempXML = System.Text.Encoding.UTF8.GetString(XML);
+            file.Close();
+            string temp = "";
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    temp += secondBox.Text + " [Электронный ресурс]/ Режим доступа: "
+                        + thirdBox.Text + "."; break;
+                case 1:
+                     temp += thirdBox.Text + ". " + secondBox.Text +
+                        " [Текст] / " + fourthBox.Text + " — " + fifthBox.Text + "с.";
+                    break;
+                default: break;
+            }
+
+            tempXML = tempXML.Replace("123456654321", temp);
+            secondBox.Text = "";
+            fourthBox.Text = "";
+            thirdBox.Text = "";
+            fifthBox.Text = "";
+
+            res += tempXML;
+
+            if (currentIndex == totalIndexes)
+            {
+                FileStream file1 = new FileStream(@"XML\last.xml", FileMode.Open);
+                XML = new byte[file1.Length];
+                file1.Read(XML, 0, XML.Length);
+                file1.Close();
+                string lastXML = System.Text.Encoding.UTF8.GetString(XML);
+                res += lastXML;
+                AddLinks();
+                MessageBoxResult result = MessageBox.Show("Работа сделана. Хотите использовать программу повторно?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    MainWindow main = new MainWindow();
+                    main.Show();
+                    this.Close();
+                }
+                else
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+        private void AddLinks()
+        {
+            WordprocessingDocument wordprocessingDocument =
+                   WordprocessingDocument.Open(filepath, true);
+            Body body = wordprocessingDocument.MainDocumentPart.Document.Body;
+
+            DocumentFormat.OpenXml.Wordprocessing.Paragraph para = body.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Paragraph());
+            DocumentFormat.OpenXml.Wordprocessing.Run run = para.AppendChild(new DocumentFormat.OpenXml.Wordprocessing.Run());
+
+            body.InnerXml += res;
+
+            wordprocessingDocument.Close();
+        }
+    }
+}
